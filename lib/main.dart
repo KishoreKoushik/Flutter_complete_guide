eb9b1ce23af4159469ace79e9229b4306a29ca9c
Kishore S R <Kishore.sr@mobinius.com>
2020-01-02 18:19:53 +0530

    Options for the questions made dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,8 @@ class MyApp extends StatefulWidget {
 //underscore makes the class private
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  void _answerQuestion() {
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print(_questionIndex);
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    var questions = [
+   final questions = const[
       {
         'questionText': 'What\'s your favourite color',
         'answers': ['Black', 'Red', 'Green', 'White']
@@ -42,17 +34,33 @@ class _MyAppState extends State<MyApp> {
         'answers': ['max', 'Max', 'Max', 'Max']
       }
     ];
+  void _answerQuestion() {
+
+    if(_questionIndex < questions.length){
+      print('object');
+    }
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
-      ),
+      ),  
       body: Column(
         children: <Widget>[
-          Question(questions[_questionIndex]),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
+          Question(
+            questions[_questionIndex]['questionText']
+            ),
+          ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
+            return Answer(_answerQuestion, answer);
+          }).toList()
         ],
       ),
     ));
